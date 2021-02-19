ea2b4bb7400416d1e965374719599be0c6151e1f
Owen Davies <owen@odavi.es>
2021-02-19 20:03:25 +0000

    Add Day 22 part 2
    Not sure if there's a clever way to work out how big the search
    space should be, but multiplying the target by two is good enough.

--- src/main/scala/advent2018/Day22.scala
+++ src/main/scala/advent2018/Day22.scala
@@ -1,14 +1,24 @@
 package advent2018
 
+import scalaadventutils.Dijkstra
+import scalaadventutils.WeightedUndirectedGraph
+
 import annotation.tailrec
 
 object Day22 {
 
     type Caves = Map[Point, Type]
-    type GL = Map[Point, Int]
+    type GL    = Map[Point, Int]
+    type State = (Type, Equipment)
+    type Graph = Map[(Point, Equipment), Map[(Point, Equipment), Int]]
+
+    private val nonDiagNeighbourList = List(
+        (-1, 0), (0, -1), (1, 0), (0, 1)
+    )
 
     def main(args: Array[String]): Unit = {
         println(part1(Point(7, 782), 11820))
+        println(part2(Point(7, 782), 11820))
     }
 
     def part1(target: Point, depth: Int): Int = {
@@ -24,9 +34,18 @@ object Day22 {
         ).sum
     }
 
+    def part2(target: Point, depth: Int): Int = {
+        val caves = constructCave(target, depth)
+        val graph = constructGraph(caves, target)
+
+        val path = Dijkstra.shortestPath(
+            graph, (Point(0, 0), Torch), (target, Torch))
+        Dijkstra.shortestPathTotalWeight(graph, path)
+    }
+
     def constructCave(target: Point, depth: Int): Caves = {
-        val maxx = target.x
-        val maxy = target.y
+        val maxx = target.x * 2
+        val maxy = target.y * 2
 
         val geologicLevels: GL =
             (0 to maxx).map(x => Point(x, 0) -> x * 16807).toMap ++
@@ -37,15 +56,15 @@ object Day22 {
 
         (0 to maxy).map(y =>
             (0 to maxx).map(x => {
-                val geologicIndex = (y, x) match {
+                val geoIndex = (x, y) match {
                     case (0, 0) => 0
-                    case (y, x) if x == target.x && y == target.y => 0
-                    case (0, x) => x * 16807
-                    case (y, 0) => y * 48271
-                    case (y, x) => erosions(y)(x - 1) * erosions(y - 1)(x)
+                    case (x, y) if x == target.x && y == target.y => 0
+                    case (0, y) => y * 48271
+                    case (x, 0) => x * 16807
+                    case (x, y) => erosions(y)(x - 1) * erosions(y - 1)(x)
                 }
 
-                erosions(y)(x) = (geologicIndex + depth) % 20183
+                erosions(y)(x) = (geoIndex + depth) % 20183
             })
         )
 
@@ -64,24 +83,84 @@ object Day22 {
         ).toMap
     }
 
+    def constructGraph
+        ( c: Caves
+        , target: Point)
+        : WeightedUndirectedGraph[(Point, Equipment)] = {
+
+        val maxx = target.x * 2
+        val maxy = target.y * 2
+
+        val graph: Graph =
+        (0 to maxy).flatMap(y =>
+            (0 to maxx).map(x => {
+                val p = Point(x, y)
+                val regionType = c(p)
+
+                regionType.usable.map(e => {
+                    // TODO: remove ugly mutability here
+                    val edges = collection.mutable.Map[(Point, Equipment), Int]()
+
+                    (regionType, e) match {
+                        case (Rocky, Torch)    => edges += (p, Climbing) -> 7
+                        case (Rocky, Climbing) => edges += (p, Torch)    -> 7
+                        case (Wet, None)       => edges += (p, Climbing) -> 7
+                        case (Wet, Climbing)   => edges += (p, None)     -> 7
+                        case (Narrow, None)    => edges += (p, Torch)    -> 7
+                        case (Narrow, Torch)   => edges += (p, None)     -> 7
+                        case _ =>
+                    }
+
+                    val neighbours = nonDiagNeighbours(p, maxx, maxy)
+                    neighbours.map(n => {
+                        val nRegionType = c(n)
+                        if (nRegionType.usable.contains(e))
+                            edges += (n, e) -> 1
+                    })
+                    (p, e) -> edges.toMap
+                })
+            })
+        ).flatten.toMap
+
+        new WeightedUndirectedGraph(graph)
+    }
+
+    def nonDiagNeighbours(p: Point, maxx: Int, maxy: Int) =
+        nonDiagNeighbourList.map(n => Point(p.x + n._1, p.y + n._2))
+                            .filter(p => p.x >= 0 && p.x <= maxx &&
+                                         p.y >= 0 && p.y <= maxy)
+
     case class Point(x: Int, y: Int)
 
     sealed trait Type {
         def risk: Int
+        def usable: List[Equipment]
     }
 
     case object Rocky extends Type {
         def risk = 0
         override def toString() = "."
+        def usable = List(Climbing, Torch)
     }
 
     case object Wet extends Type {
         def risk = 1
         override def toString() = "="
+        def usable = List(Climbing, None)
     }
 
     case object Narrow extends Type {
         def risk = 2
         override def toString() = "|"
+        def usable = List(None, Torch)
     }
+
+    sealed trait Equipment
+
+    case object Torch extends Equipment
+
+    case object Climbing extends Equipment
+
+    case object None extends Equipment
 }
+
--- src/test/scala/Day22.scala
+++ src/test/scala/Day22.scala
@@ -9,4 +9,10 @@ class Day22Spec extends AnyFunSuite {
             Day22.part1(Day22.Point(10, 10), 510)
         }
     }
+
+    test("Day 22: part2") {
+        assertResult(45) {
+            Day22.part2(Day22.Point(10, 10), 510)
+        }
+    }
 }
