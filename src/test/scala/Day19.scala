d4bafc5fce0c8a117f6f50d29c29fd404a78e219
Owen Davies <owen@odavi.es>
2021-02-21 09:53:05 +0000

    Fix test input

--- src/test/scala/Day19.scala
+++ src/test/scala/Day19.scala
@@ -9,7 +9,7 @@ class Day19Spec extends AnyFunSuite {
     test("Day 19: run") {
         val lines = Problem.parseInputToList("day19-test")
         assertResult(7) {
-            Day19.run(0, Map[Int, Int](), lines)
+            Day19.run(0, Map[Int, Int]().withDefaultValue(0), lines)
         }
     }
 }
